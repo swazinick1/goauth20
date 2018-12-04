c3b2f3fc9fb2424733ae9ce0d62dc75e7ab0733f
Swazinick <swazinick1@gmail.com>
2018-12-03 19:08:05 -0600

    taking a step back from production

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,9 @@
 // prod.js - Production keys!!!
 
-module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
-};
+// module.exports = {
+//     googleClientID: process.env.GOOGLE_CLIENT_ID,
+//     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     mongoURI: process.env.MONGO_URI,
+//     cookieKey: process.env.COOKIE_KEY
+// };
+
