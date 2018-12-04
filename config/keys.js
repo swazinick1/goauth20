a6770b6235c74da16c4d13296343446ed4204953
Swazinick <swazinick1@gmail.com>
2018-12-03 20:04:49 -0600

    bug fixes

--- config/keys.js
+++ config/keys.js
@@ -1,16 +1,11 @@
 // keys.js figure out what set of creds to return??
 
-module.exports = {
-    googleClientID: '335509474849-0f3gvq8nkj9fsivs3tle7802j4pf6p0l.apps.googleusercontent.com',
-    googleClientSecret: 't78o1ti0B4k5_4pKokbDtlfQ',
-    mongoURI: 'mongodb://swazinick1:Single420@ds229732.mlab.com:29732/logins',
-    cookieKey: 'hjsdhfjasdhflakdsh'
-};
 
-// if (process.env.NODE_ENV === 'production') {
-//     // we are in production - return the prod est of keys
-//     module.exports = require('./prod');
-// } else {
-//     // we are in development - return the dev keys!!!
-//     module.exports = require('./dev');
-// }
+
+if (process.env.NODE_ENV === 'production') {
+    // we are in production - return the prod est of keys
+    module.exports = require('./prod');
+} else {
+    // we are in development - return the dev keys!!!
+    module.exports = require('./dev');
+}
--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,10 @@
 // prod.js - Production keys!!!
 
-// module.exports = {
-//     googleClientID: process.env.GOOGLE_CLIENT_ID,
-//     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     mongoURI: process.env.MONGO_URI,
-//     cookieKey: process.env.COOKIE_KEY
-// };
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
 
